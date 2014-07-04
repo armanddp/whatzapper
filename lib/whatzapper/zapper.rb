c9e7467adae83f540616c9b4f409dd298e5b8bd8
Armand du Plessis <adp@bank.io>
2014-07-04 20:34:49 +0400

    Move db to writable space for updates

--- lib/whatzapper/zapper.rb
+++ lib/whatzapper/zapper.rb
@@ -1,4 +1,5 @@
 require 'sqlite3'
+require 'tmpdir'
 
 module Whatzapper
   class Zapper
@@ -6,11 +7,18 @@ module Whatzapper
     attr_accessor :path
 
     def initialize(path)
-      @path = path
+      @path = Pathname.new path
     end
 
-    def zap
-      db = open_db
+    def clean_db
+      work_in_writable_space do |working_db|
+        zap(working_db)
+      end
+    end
+
+    protected
+    def zap(db)
+      db = open_db(db)
       open_chats(db) do |message|
         pk, text = message['Z_PK'], message['ZTEXT']
         next unless text =~ /(fi|if)/
@@ -20,8 +28,8 @@ module Whatzapper
     end
 
     private
-    def open_db
-      SQLite3::Database.new(@path)
+    def open_db(db)
+      SQLite3::Database.new(db)
     end
 
     def open_chats(db)
@@ -30,12 +38,21 @@ module Whatzapper
       end
     end
 
+    def work_in_writable_space
+      Dir.mktmpdir do |tmp_dir|
+        FileUtils.cp self.path.realpath, tmp_dir
+        working_db = (Pathname.new(tmp_dir) + self.path.basename).to_s
+        yield working_db
+        FileUtils.cp working_db, self.path
+      end
+    end
+
     def clean_message(db, pk, text)
-      cleaned_text = text.sub(/(fi|if)/) do |match|
+      cleaned_text = text.sub(/(fi|if|ff)/) do |match|
         "#{match[0]}*"
       end
       puts "cleaned: #{cleaned_text}"
-      puts db.execute("UPDATE ZWAMESSAGE SET ZTEXT = '#{cleaned_text}' WHERE Z_PK = #{pk};")
+      db.execute("UPDATE ZWAMESSAGE SET ZTEXT = '#{cleaned_text}' WHERE Z_PK = #{pk};")
     end
   end
 end
